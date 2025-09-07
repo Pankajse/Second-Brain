85273c1a66aa7667f4679adcfee71bacf370e625
Pankaj42se <pankaj42se@gmail.com>
2025-09-07 16:03:01 +0530

    Backend changes

--- Backend/src/config.ts
+++ Backend/src/config.ts
@@ -4,9 +4,7 @@ import "dotenv/config"
 const envSchema = z.object({
     JWT_SECRET : z.string().min(3,"Minimum Length of JWT_SECRET is 3"),
     DB_URL : z.string().min(3,"Minimum Length of DB_URL is 3"),
-    PORT : z.string().transform(Number),
-    EMAIL : z.string(),
-    PASSWORD : z.string()
+    PORT : z.string().transform(Number)
 });
 
 const parsed = envSchema.parse(process.env);
@@ -14,7 +12,5 @@ const parsed = envSchema.parse(process.env);
 export const config = {
     JWT_SECRET : parsed.JWT_SECRET,
     DB_URL : parsed.DB_URL,
-    PORT : parsed.PORT,
-    email : parsed.EMAIL,
-    password : parsed.PASSWORD
+    PORT : parsed.PORT
 }
